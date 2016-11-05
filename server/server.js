f2c2c029bf228e3c4b25435eb69c66653db22b62
Thanh Le <lenguyenthanh@hotmail.com>
2016-11-05 11:50:03 +0700

    Fix server.js code style

--- server/server.js
+++ server/server.js
@@ -21,9 +21,12 @@ app.start = function() {
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname, function(err) {
-  if (err) throw err;
+  if (err) {
+    throw err;
+  }
 
   // start the server if `$ node server.js`
-  if (require.main === module)
+  if (require.main === module)i {
     app.start();
+  }
 });
