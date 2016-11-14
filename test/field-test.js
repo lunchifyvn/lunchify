bc06a487ebf24d4aa4ce2685a16a9d1533b70268
Nguyễn Quốc Đính <nqd@users.noreply.github.com>
2016-11-14 21:11:35 +0700

    Field acl fake test (#18)
    * add fake test
    
    * allow unuse var with _ prefix
    
    * add second test, use should
    
    * adding gulp to watch for performing test
    
    * need clientID and clientSecret default to be not null

---
+++ test/field-test.js
@@ -0,0 +1,28 @@
+var app = require('../server/server');
+var should = require('should');
+
+function req(verb, url) {
+  return require('supertest')(app)[verb](url)
+    .set('Content-Type', 'application/json')
+    .set('Accept', 'application/json')
+    .expect('Content-Type', /json/);
+};
+
+describe('Field API', () => {
+  it.skip('should not allow annonymous user to get fields', done => {
+    req('get', '/api/fields')
+    .expect(401, (err, _res) => {
+      should.ifError(err);
+      done();
+    });
+  });
+
+  it.skip('should allow registered user to get fields', done => {
+    req('get', '/api/fields')
+    .expect(200, (err, res) => {
+      should.ifError(err);
+      res.should.be.an.Array();
+      done();
+    });
+  });
+});
