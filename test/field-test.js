5b9b51b94531ebec3b533e5a32d2272ed4c56c04
nqdinh <nqdinhddt@gmail.com>
2016-11-14 22:13:26 +0700

    only authenticated user can find fields

--- test/field-test.js
+++ test/field-test.js
@@ -1,5 +1,6 @@
 var app = require('../server/server');
 var should = require('should');
+var User = app.models.user;
 
 function req(verb, url) {
   return require('supertest')(app)[verb](url)
@@ -9,6 +10,23 @@ function req(verb, url) {
 };
 
 describe('Field API', () => {
+  var user1 = {
+    email: 'user1@dev.dev',
+    username: 'user1',
+    password: 'user1pass',
+  };
+  var user1User;
+  before(done => {
+    User.upsert(user1, () => {
+      req('post', '/api/users/login')
+      .send(user1)
+      .expect(200, function(err, res) {
+        user1User = res.body;
+        done(err);
+      });
+    });
+  });
+
   it('should not allow annonymous user to get fields', done => {
     req('get', '/api/fields')
     .expect(401, (err, _res) => {
@@ -17,11 +35,11 @@ describe('Field API', () => {
     });
   });
 
-  it.skip('should allow registered user to get fields', done => {
-    req('get', '/api/fields')
+  it('should allow registered user to get fields', done => {
+    req('get', `/api/fields?access_token=${user1User.id}`)
     .expect(200, (err, res) => {
       should.ifError(err);
-      res.should.be.an.Array();
+      res.should.have.property('body').which.is.an.Array();
       done();
     });
   });
