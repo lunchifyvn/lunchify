2c895c66c38dffcb2bece3182c58c798f53e91d0
tuan072090 <tuannmuit@gmail.com>
2016-11-19 10:43:26 +0700

    change index, gulpfile

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -2,12 +2,16 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
+  var newDate = new Date();
+  console.log(newDate+" -- Compile scss to css");
   return gulp.src('./src/scss/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(gulp.dest('./assets/css'));
 });
 
 gulp.task('watch', function () {
+  var newDate = new Date();
+  console.log(newDate+" -- Content change");
   gulp.watch('./src/scss/*.scss', ['sass']);
 });
 
