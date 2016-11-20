a8fa5075b4b7055192e24c6531bacb591ee20d42
nqdinh <nqdinhddt@gmail.com>
2016-11-20 11:41:34 +0700

    suggestion skeleton fnc

--- common/models/user.js
+++ common/models/user.js
@@ -1,4 +1,17 @@
+/**
+ * suggestion function
+ * define a user as
+ * {userId: id, prefers: [], location: {}}
+ * prefer: {type: field/topic, id: id}
+ *
+ * @param {any} origin user
+ * @param {any} group [user]
+ *
+ * returns an array of user: [user]
+ */
+function suggestion(origin, group) {
 
+}
 module.exports = function(User) {
   User.prototype.suggestion = function(cb) {
     console.log('prefer');
