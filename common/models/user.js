21afbd22584bfb5e42acc53bca6d5eb56c57aa80
nqdinh <nqdinhddt@gmail.com>
2016-11-20 12:36:56 +0700

    cleanup the matching function

--- common/models/user.js
+++ common/models/user.js
@@ -1,19 +1,4 @@
-/**
- * suggestion function
- * define a user as
- * {id: id, prefers: [], location: {}}
- * prefer: {type: field/topic, id: id}
- *
- * @param {any} origin user
- * @param {any} group [user]
- *
- * returns an array of user: [user]
- */
-function matchProfile(origin, group) {
-  console.log('origin', origin);
-  console.log('group', group);
-  return group;
-}
+var matching = require('../../lib/matching');
 
 module.exports = function(User) {
   User.prototype.suggestion = function(cb) {
@@ -34,7 +19,7 @@ module.exports = function(User) {
         if (err) {
           return cb(err);
         }
-        var suggestion = matchProfile(userProfile, users);
+        var suggestion = matching.matchProfile(userProfile, users);
         cb(null, suggestion);
       });
     });
