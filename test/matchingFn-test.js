ab11f987706c7b3bfd23b3fd8dfbf85271efcf3e
Thanh Le <lenguyenthanh@hotmail.com>
2016-11-21 20:05:45 +0700

    Filter maching users by theirs location

--- test/matchingFn-test.js
+++ test/matchingFn-test.js
@@ -2,6 +2,28 @@ var matchingFn = require('../lib/matching');
 require('should');
 
 describe('Matching Profile Fn', () => {
+  it('should check distance between 2 users', () => {
+    var origin = {
+      userId: 1,
+      location: {
+        lat: 0,
+        long: 0,
+      },
+    };
+
+    var group = [{
+      userId: 2,
+      location: {
+        lat: 10.4,
+        long: 108.4,
+      },
+    }];
+
+    var matchedProfile = matchingFn.matchProfile(origin, group, 1000);
+    matchedProfile.should.be.an.Array();
+    matchedProfile.should.be.empty();
+  });
+
   it('should provide basic matching for field with all users', () => {
     var origin = {
       prefers: [
@@ -73,8 +95,49 @@ describe('Matching Profile Fn', () => {
       },
     }];
 
-    var matchedProfile = matchingFn.matchProfile(origin, group);
+    var matchedProfile = matchingFn.matchProfile(origin, group, 1000);
     matchedProfile.should.be.an.Array();
     matchedProfile.should.deepEqual([group[1]]);
+  });
+
+  it('should check distance and prefers', () => {
+    var origin = {
+      prefers: [
+        {type: 'field', ref: 7, userId: 1, id: 1},
+        {type: 'field', ref: 112, userId: 1, id: 36},
+      ],
+      userId: 1,
+      location: {
+        lat: 10.830876,
+        long: 106.680333,
+      },
+    };
+
+    var group = [
+      {
+        prefers: [
+          {type: 'field', ref: 7, userId: 3, id: 13},
+        ],
+        userId: 2,
+        location: {
+          lat: 10.830795,
+          long: 106.680532,
+        },
+      },
+      {
+        prefers: [
+          {type: 'field', ref: 7, userId: 3, id: 13},
+        ],
+        userId: 3,
+        location: {
+          lat: 10.4,
+          long: 108.4,
+        },
+      },
+    ];
+
+    var matchedProfile = matchingFn.matchProfile(origin, group, 1000);
+    matchedProfile.should.be.an.Array();
+    matchedProfile.should.deepEqual([group[0]]);
   });
 });
