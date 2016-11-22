9d258030306c97f35a9ccadda5ae1a17d57ced36
nqdinh <nqdinhddt@gmail.com>
2016-11-22 17:21:54 +0700

    should allow one user get another identity

--- test/user-test.js
+++ test/user-test.js
@@ -9,20 +9,36 @@ function req(verb, url) {
     .expect('Content-Type', /json/);
 };
 
+var user1 = {
+  email: 'user1@dev.dev',
+  username: 'user1',
+  password: 'user1pass',
+};
+var user2 = {
+  email: 'user2@dev.dev',
+  username: 'user2',
+  password: 'user2pass',
+};
+
+var user1User, user2User;
+
 describe('User select API', () => {
-  var user1 = {
-    email: 'user1@dev.dev',
-    username: 'user1',
-    password: 'user1pass',
-  };
-  var user1User;
   before(done => {
+    // user1
     User.upsert(user1, () => {
       req('post', '/api/users/login')
       .send(user1)
       .expect(200, function(err, res) {
         user1User = res.body;
-        done(err);
+        // user2
+        User.upsert(user2, () => {
+          req('post', '/api/users/login')
+          .send(user2)
+          .expect(200, function(err, res) {
+            user2User = res.body;
+            done(err);
+          });
+        });
       });
     });
   });
@@ -30,6 +46,15 @@ describe('User select API', () => {
   it('should allow user to get the identities', done => {
     req('get',
     `/api/users/${user1User.userId}/identities?access_token=${user1User.id}`)
+    .expect(200, (err, _res) => {
+      should.ifError(err);
+      done();
+    });
+  });
+
+  it('should allow user2 to get user1 identity', done => {
+    req('get',
+    `/api/users/${user1User.userId}/identities?access_token=${user2User.id}`)
     .expect(200, (err, _res) => {
       should.ifError(err);
       done();
