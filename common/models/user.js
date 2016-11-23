b2f2affae476ee50acf956f9f9ff1eb15433f951
nqdinh <nqdinhddt@gmail.com>
2016-11-23 19:44:28 +0700

    fully extract user profile

--- common/models/user.js
+++ common/models/user.js
@@ -3,28 +3,36 @@ var matching = require('../../lib/matching');
 module.exports = function(User) {
   User.prototype.suggestion = function(cb) {
     // get the profile of this user
-    this.prefers((err, instances) => {
-      var userProfile = {
-        prefers: instances,
-        id: this.id,
-        location: {long: 0, lat: 0},
-      };
+    this.locations.findOne({
+      order: 'createdAt DESC',
+    }, (err, location) => {
+      if (err) {
+        return cb(err);
+      }
+      this.prefers((err, perfers) => {
+        var userProfile = {
+          prefers: perfers,
+          id: this.id,
+          location: location,
+        };
 
-      // get the profile of other users
-      User.find({
-        where: {id: {neq: userProfile.id}}, // except the user himself
-        include: ['prefers', 'locations'], // include the prefers
-        fields: {
-          id: true,
-          prefers: true,
-          locations: true,
-        }, // select id and prefer and location
-      }, (err, users) => {
-        if (err) {
-          return cb(err);
-        }
-        var suggestion = matching.matchProfile(userProfile, users);
-        cb(null, suggestion);
+        // get the profile of other users
+        User.find({
+          where: {id: {neq: userProfile.id}}, // except the user himself
+          include: ['prefers', 'locations'], // include the prefers
+          fields: {
+            id: true,
+            prefers: true,
+            locations: true,
+          }, // select id and prefer and location
+        }, (err, users) => {
+          if (err) {
+            return cb(err);
+          }
+          // finally, find the suggestion
+          var suggestion = matching.matchProfile(userProfile, users);
+          cb(null, suggestion);
+        });
       });
     });
   };
